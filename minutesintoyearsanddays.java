cb0856c324cdb04526a4a0c9b1ac6cb2d09294c1
Meghna-U <82393429+Meghna-U@users.noreply.github.com>
2021-06-04 18:07:13 +0530

    Update minutesintoyearsanddays.java

--- minutesintoyearsanddays.java
+++ minutesintoyearsanddays.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 
 
 public class MinutesIntoYearsAndDays {
-
-    
-    public static void main(String[] args) {
-        int min,yrs,days;
+    public static void convert(int min) {
+        int yrs,days;
         float x;
-        System.out.println("Input the number of minutes:");
-        Scanner s=new Scanner(System.in);
-        min=s.nextInt();
         yrs=min/525600;
         x=(float)min/525600;
         days=(int) ((x-yrs)*365);
         System.out.println(min+" minutes is approximately "+yrs+" years and "+days+" days");
+    }
+
+    
+    public static void main(String[] args) {
+        int min;
+        System.out.println("Input the number of minutes:");
+        Scanner s=new Scanner(System.in);
+        min=s.nextInt();
+        convert(min);
     }
     
 }
